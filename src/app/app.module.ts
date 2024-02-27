b359eab1f649339fc23754705d8aaefe697dc586
izadoranetz <izadora.ns@gmail.com>
2024-02-26 23:48:26 -0300

    Update user profile and repository list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { RepositoryListComponent } from './repository-list/repository-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { RepositoryListComponent } from './repository-list/repository-list.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/repository-list/repository-list.component.ts
+++ src/app/repository-list/repository-list.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-repository-list',
   templateUrl: './repository-list.component.html',
   styleUrl: './repository-list.component.scss'
 })
-export class RepositoryListComponent {
-
+export class RepositoryListComponent{
+  @Input() repositories: any[] | undefined;
 }
